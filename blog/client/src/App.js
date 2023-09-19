21de2d6637e15f29f5266cd106171b01488b6736
kasturada1 <kazuki1.katsurada@gmail.com>
2023-09-19 22:20:18 +0900

    Build post submission

--- blog/client/src/App.js
+++ blog/client/src/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import PostCreate from './PostCreate';
 
-export default () => {
-  return <div>Blog app</div>;
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Create Post</h1>
+      <PostCreate />
+    </div>
+  );
 };
+export default App;
