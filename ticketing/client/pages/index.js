ee0f9876d1022d0dffac790d87ac676d1e7fd0a0
kasturada1 <kazuki1.katsurada@gmail.com>
2023-10-14 16:21:06 +0900

    Add an onSuccess callback

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,3 +1,12 @@
-export default () => {
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+  axios.get('/api/users/currentuser').catch((err) => {
+    console.log(err.message);
+  });
+
   return <h1>Landing Page</h1>;
 };
+
+export default LandingPage;
