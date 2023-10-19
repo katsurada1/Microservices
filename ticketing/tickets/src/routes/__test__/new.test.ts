ef94d5283a52fdd9fa29382b5feadc8c33922acd
kasturada1 <kazuki1.katsurada@gmail.com>
2023-10-19 19:23:50 +0900

    Add validating test for title and price

--- ticketing/tickets/src/routes/__test__/new.test.ts
+++ ticketing/tickets/src/routes/__test__/new.test.ts
@@ -20,8 +20,52 @@ it('returns a status other than 401 if the user is signed in', async () => {
   expect(response.status).not.toEqual(401);
 });
 
-it('returns an error if an invalid title is provided', async () => {});
+it('returns an error if an invalid title is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: '',
+      price: 10,
+    })
+    .expect(400);
 
-it('returns an error if an invalid price is provided', async () => {});
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      price: 10,
+    })
+    .expect(400);
+});
 
-it('creates a ticket with valid inputs', async () => {});
+it('returns an error if an invalid price is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'asldkjf',
+      price: -10,
+    })
+    .expect(400);
+
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'laskdfj',
+    })
+    .expect(400);
+});
+
+it('creates a ticket with valid inputs', async () => {
+  // add in a check to make sure a ticket was saved
+
+  await request(app)
+    .post('/api/tickets')
+    .send({
+      title: 'asldkfj',
+      price: 20,
+    })
+    .expect(201);
+});
