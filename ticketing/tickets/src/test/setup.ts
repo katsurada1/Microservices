bc5952350ab8ef648a12b9b1eb3159e9a754fda7
kasturada1 <kazuki1.katsurada@gmail.com>
2023-10-27 19:41:19 +0900

    Ensure mock invocations

--- ticketing/tickets/src/test/setup.ts
+++ ticketing/tickets/src/test/setup.ts
@@ -22,6 +22,7 @@ beforeAll(async () => {
 });
 
 beforeEach(async () => {
+  jest.clearAllMocks();
   const collections = await mongoose.connection.db.collections();
 
   for (let collection of collections) {
@@ -37,24 +38,13 @@ afterAll(async () => {
 });
 
 global.signin = () => {
-  // Build a JWT payload.  { id, email }
   const payload = {
     id: new mongoose.Types.ObjectId().toHexString(),
     email: 'test@test.com',
   };
-
-  // Create the JWT!
   const token = jwt.sign(payload, process.env.JWT_KEY!);
-
-  // Build session Object. { jwt: MY_JWT }
   const session = { jwt: token };
-
-  // Turn that session into JSON
   const sessionJSON = JSON.stringify(session);
-
-  // Take JSON and encode it as base64
   const base64 = Buffer.from(sessionJSON).toString('base64');
-
-  // return a string thats the cookie with the encoded data
   return [`session=${base64}`];
 };
